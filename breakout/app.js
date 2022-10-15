a104cbaf5e965c7509adc0fd166ca628008e5b4a
edmilson <mangueleedmilson@gmail.com>
2022-10-15 21:06:00 +0200

    added a new folder

---
+++ breakout/app.js
@@ -0,0 +1,13 @@
+// revising classes
+
+class Rectangle {
+    
+    constructor () {
+        let height = 20; 
+        let base = 50 ;
+        console.log('the rectangle has been created'); 
+    }
+}
+
+const newRect = new Rectangle; 
+console.log(newRect.base)
